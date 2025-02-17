f5cf449f61d94dda4c8056b9ae7c03890b271661
LUCAS SETTE QUEIROZ <136368302+Lucassette7@users.noreply.github.com>
2025-02-17 11:00:18 -0300

    d

--- calculadora/index.js
+++ calculadora/index.js
@@ -4,21 +4,23 @@ function adicionarTela(valor){
     document.getElementById('tela').innerText = numeros
 }
 
+function limpar (){
+    document.getElementById('tela').innerText = ''
+    numeros = ''
+}
 
 function calculo() {
     try {
         // Calcula a expressão usando eval
         const resultado = eval(numeros);
         document.getElementById('tela').innerText = resultado
-        limpar()
+        numeros = ''
     } catch (erro) {
         console.error('Erro no cálculo:', erro); // Se ocorrer um erro, exibe no console
     }
 }
 
-function limpar (){
-    numeros = ''
-}
+
 
 
 
